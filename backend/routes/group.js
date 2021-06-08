2c83025640d5d842195df21419cda1bd520b1dce
Fizza-Rubab <fr06161@st.habib.edu.pk>
2021-06-09 01:48:06 +0500

    renaming done

--- backend/routes/group.js
+++ backend/routes/group.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 let Group = require("../models/group.model");
 
 const { catchErrors } = require("../handlers/errorHandlers");
-const groupController = require("../controllers/userController");
+const groupController = require("../controllers/groupController");
 router.get("/", catchErrors(groupController.getGroups));
 router.post("/create", catchErrors(groupController.createGroup));
 router.get("/:id", catchErrors(groupController.findGroup));
 router.delete("/delete/:id", catchErrors(groupController.deleteGroup));
 router.get("/status/:id", catchErrors(groupController.status));
-router.get("/update/:id", catchErrors(groupController.updateGroup));
+router.put("/update/:id", catchErrors(groupController.updateGroup));
 
 export default router;
