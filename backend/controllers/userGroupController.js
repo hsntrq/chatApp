bff9ab94e5ed4786f97b8c5345389ddda797495c
Fizza-Rubab <fr06161@st.habib.edu.pk>
2021-06-09 22:02:54 +0500

    extra route added

--- backend/controllers/userGroupController.js
+++ backend/controllers/userGroupController.js
@@ -1,5 +1,12 @@
+const mongoose = require("mongoose");
 const router = require("express").Router();
 let UserGroup = require("../models/userGroup.model");
+
+exports.getAll = async (req, res) => {
+  UserGroup.find()
+    .then((userGroups) => res.json(userGroups))
+    .catch((err) => res.status(400).json("Error: " + err));
+};
 
 // get all groups in which user is a member(may aswell be an admin)
 exports.memberOf = async (req, res) => {
@@ -21,8 +28,10 @@ exports.joinGroup = async (req, res) => {
   const newConnection = new UserGroup({
     userID: req.body.userID,
     groupID: req.body.groupID,
+    isAdmin: req.body.isAdmin,
   });
-  await newConnection
+  console.log(newConnection.userID);
+  newConnection
     .save()
     .then(() => res.json("User has joined the group!"))
     .catch((err) => res.status(400).json("Error: " + err));
--- backend/routes/userGroup.js
+++ backend/routes/userGroup.js
@@ -3,6 +3,7 @@ let UserGroup = require("../models/userGroup.model");
 
 const { catchErrors } = require("../handlers/errorHandlers");
 const userGroupController = require("../controllers/userGroupController");
+router.get("/", catchErrors(userGroupController.getAll));
 router.get("/:userid", catchErrors(userGroupController.memberOf));
 router.get("/admin/:userid", catchErrors(userGroupController.isAdminOf));
 router.post("/join", catchErrors(userGroupController.joinGroup));
