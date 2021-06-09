d555cd59cc2f482357ad253b4bcb98e9f16b1e62
hasan <hasannaseem2@gmail.com>
2021-06-09 17:59:48 +0500

    userController completed and tested by hasan

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 const mongoose = require('moongoose');
 const User = require("../models/user.model");
 const sha256 = require("js-sha256");
-
+const jwt = require("jwt-then");
 
 exports.register = async(req, res) => {
     const { userName, firstName, lastName, email, password, gender, isAdmin } = req.body;
@@ -11,6 +11,13 @@ exports.register = async(req, res) => {
     if (!emailRegex.test(email)) throw "Email is not supported from your domain";
     if (typeof firstName !== 'string') throw "firstname must contain only alphabets";
     if (typeof lastName !== 'string') throw "lastname must contain only alphabets";
+
+
+    const userExists = await User.findOne({
+        email,
+        userName
+    });
+    if (userExists) throw "User with same username and email already exists";
 
 
     const user = new User({
@@ -28,4 +35,81 @@ exports.register = async(req, res) => {
     res.json({
         message: "User [" + firstName + "] registered successfully!"
     });
+}
+
+exports.login = async(req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+        email,
+        password: sha256(password + process.env.SALT),
+    });
+
+    if (!user) throw "incorrect email and password entered";
+
+    const token = await jwt.sign({ id: user.id }, process.env.SECRET);
+
+    res.json({
+        message: "User logged in successfully",
+        token
+    })
+}
+
+exports.findAllUsers = async(req, res) => {
+    let query = req.query.q;
+
+    if (!query) query = ".*";
+    const users = await User.find({
+        $or: [
+            { userName: { $regex: query, $options: "i" } },
+            { firstName: { $regex: query, $options: "i" } },
+            { lastName: { $regex: query, $options: "i" } },
+            { email: { $regex: query + ".*(?=@)", $options: "i" } },
+        ],
+    });
+
+    if (!users) throw "No users currently exist"
+    res.json({
+        users
+    })
+
+}
+
+exports.findUserById = async(req, res) => {
+    const user = await User.findById(req.params.id);
+    if (!user) throw "No user exists with this id"
+    res.json({ user })
+}
+
+exports.checkUserStatus = async(req, res) => {
+    const user = await User.findById(req.params.id);
+    if (!user) throw "No user exists with this id"
+    res.json(
+        user.status
+    )
+}
+
+exports.deleteUser = async(req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id)
+    if (!user) throw "User does not exist"
+    res.json("User successfully deleted")
+}
+
+exports.updateUser = async(req, res) => {
+    var updatedRecord = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+        email: req.body.email,
+        gender: req.body.gender,
+        image: req.body.image,
+        phoneNo: req.body.phoneNo,
+        dateOfBirth: req.body.dateOfBirth,
+        address: req.body.address,
+        description: req.body.description,
+    };
+    User.findByIdAndUpdate(req.params.id, { $set: updatedRecord }, (err, doc) => {
+        if (!err) res.json("User UPDATED.");
+        else res.status(400).json("Error: " + err);
+    });
+
 }
