537eec5af6dcec7d4e609c60516455fbc00c9bef
hasan <hasannaseem2@gmail.com>
2021-06-10 16:57:13 +0500

    group message controlelr completed

--- backend/routes/groupMessage.js
+++ backend/routes/groupMessage.js
@@ -1,44 +1,52 @@
 const router = require("express").Router();
 let GroupMessage = require("../models/groupMessage.model");
 
-router.route("/").get((req, res) => {
-  GroupMessage.find()
-    .then((groupMessages) => res.json(groupMessages))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
+const { catchErrors } = require("../handlers/errorHandlers");
+const groupmessagecontroller = require("../controllers/groupmessageController");
+router.get("/", catchErrors(groupmessagecontroller.getAllMessages))
+router.post("/add", catchErrors(groupmessagecontroller.addMessage))
+router.delete("/:id", catchErrors(groupmessagecontroller.deleteMessage))
+router.put("/edit/:id", catchErrors(groupmessagecontroller.updateMessage))
 
-router.route("/add").post((req, res) => {
-  const newGroupMessage = new groupMessage({
-    senderID: req.body.senderID,
-    groupID: req.body.groupID,
-    messageText: req.body.messageText,
-    parentMsgId: req.body.parentMsgId,
-  });
+// router.route("/").get((req, res) => {
+//     GroupMessage.find()
+//         .then((groupMessages) => res.json(groupMessages))
+//         .catch((err) => res.status(400).json("Error: " + err));
+// });
 
-  newGroupMessage
-    .save()
-    .then(() => res.json(" Group Message added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
+// router.route("/add").post((req, res) => {
+//     const newGroupMessage = new groupMessage({
+//         senderID: req.body.senderID,
+//         groupID: req.body.groupID,
+//         messageText: req.body.messageText,
+//         parentMsgId: req.body.parentMsgId,
+//     });
 
-router.route("/:id").delete((req, res) => {
-  GroupMessage.findByIdAndDelete(req.params.id)
-    .then(() => res.json("Message deleted."))
-    .catch((err) => res.status(400).json("Error: " + err));
-});
+//     newGroupMessage
+//         .save()
+//         .then(() => res.json(" Group Message added!"))
+//         .catch((err) => res.status(400).json("Error: " + err));
+// });
 
-router.put("/edit/:id", (req, res) => {
-  var updatedRecord = {
-    sendTime: new Date().getTime(),
-    messageText: req.body.messageText,
-  };
-  GroupMessage.findByIdAndUpdate(
-    req.params.id,
-    { $set: updatedRecord },
-    (err, doc) => {
-      if (!err) res.json("Group Message Edited.");
-      else res.status(400).json("Error: " + err);
-    }
-  );
-});
+// router.route("/:id").delete((req, res) => {
+//     GroupMessage.findByIdAndDelete(req.params.id)
+//         .then(() => res.json("Message deleted."))
+//         .catch((err) => res.status(400).json("Error: " + err));
+// });
+
+// router.put("/edit/:id", (req, res) => {
+//     var updatedRecord = {
+//         sendTime: new Date().getTime(),
+//         messageText: req.body.messageText,
+//     };
+//     GroupMessage.findByIdAndUpdate(
+//         req.params.id, { $set: updatedRecord },
+//         (err, doc) => {
+//             if (!err) res.json("Group Message Edited.");
+//             else res.status(400).json("Error: " + err);
+//         }
+//     );
+// });
+
+
 module.exports = router;
